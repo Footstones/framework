bd084a50d9166072c665ab487a195cf4ce5ae5ea
Wellming Li <wellming.li@howzhi.com>
2016-02-16 16:03:59 +0800

    add testcaseconnection

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # footstone framework
+
+## CHANGELOG
+
+v0.2.4 (2016-2-16)
+
+  * Add TestCaseConnection.
---
+++ src/Dao/TestCaseConnection.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Footstones\Framework\Dao;
+
+class TestCaseConnection extends Connection
+{
+    private $_insertedTables = array();
+
+    public function insert($tableName, array $data, array $types = array())
+    {
+        $this->_insertedTables[] = $tableName;
+        return parent::insert($tableName, $data, $types);
+    }
+
+    public function getInsertedTables()
+    {
+        return $this->_insertedTables;
+    }
+
+    public function resetInsertedTables()
+    {
+        $this->_insertedTables = array();
+    }
+}
