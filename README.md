164422fa1d433e10e3323eb3087910ef0c2fcd48
Wellming Li <wellming.li@howzhi.com>
2016-02-24 00:17:47 +0800

    add error code

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ## CHANGELOG
 
+v0.2.5 (2016-2-24)
+  * Add ErrorCode.
+  * Update vendor.
+
 v0.2.4 (2016-2-16)
 
   * Add TestCaseConnection.
---
+++ src/Exception/ErrorCode.php
@@ -0,0 +1,21 @@
+<?php
+namespace Footstones\Framework\Exception;
+
+class ErrorCode
+{
+    /**
+     * 资源不存在
+     */
+    const NOT_FOUND = 101;
+
+    /**
+     * 参数缺失
+     */
+    const PARAM_MISSING = 102;
+
+    /**
+     * 参数无效
+     */
+    const PARAM_INVALID = 103;
+
+}
